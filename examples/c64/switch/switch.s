e0a043e3c2e0ba5880564108ec34fd2f9be15090
John Hammarberg <jh@binarybone.com>
2023-01-11 00:28:18 -0500

    Switch c64 example added

---
+++ examples/c64/switch/switch.s
@@ -0,0 +1,26 @@
+﻿#include "c2/c64/c64.s"
+#include "c2/c64/kernal.s"
+
+			// Simple project specific switch example
+
+			var message = "build with: c2 --message \"hello world\""
+
+			c2_cmd.invoke("--message", [&](int arga, const char *argc[])
+			{
+				message = argc[0];
+			},1);	// One argument expected
+			
+			basic_startup
+			
+			ldx #0
+			
+:			lda text,x
+			jsr KERNAL_CHROUT
+			inx
+			cpx #text.end-text
+			bne -
+
+:			rts
+		
+text:		petscii message
+.end:
