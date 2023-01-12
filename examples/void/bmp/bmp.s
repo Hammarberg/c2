bf1eb59711106b5972de863c477606d25c52e125
Hammarberg <jh@binarybone.com>
2023-01-12 09:33:02 -0500

    Improved effect in example

--- examples/void/bmp/bmp.s
+++ examples/void/bmp/bmp.s
@@ -3,6 +3,7 @@
 			// Render a BMP image using c2
 			
 #define dist(x1,y1,x2,y2) sqrt(((x1)-(x2))*((x1)-(x2))+((y1)-(y2))*((y1)-(y2)))
+#define TAU (3.14159265359 * 2.0)
 
 			macro byte @n
 			{
@@ -29,15 +30,15 @@ head:
 			dword 0	// App specific
 			dword bitmap-head	// Offset to bitmap data
 dib_head:
-			dword bitmap-dib_head	//DIB header size
+			dword bitmap-dib_head	// DIB header size
 			dword WIDTH
 			dword HEIGHT
 			word 1	// Color planes
 			word 24	// Bits per pixel
-			dword 0	//BI_RGB
-			dword end-bitmap	//Bitmap data size
-			dword 2835	//Pixels/m X
-			dword 2835	//Pixels/m Y
+			dword 0	// BI_RGB
+			dword end-bitmap	// Bitmap data size
+			dword 2835	// Pixels/m X
+			dword 2835	// Pixels/m Y
 			dword 0	// Num colors in palette
 			dword 0	// Important colors, 0 means all are
 bitmap:
@@ -45,13 +46,15 @@ bitmap:
 			{
 				for(int x = 0; x<WIDTH; x++)
 				{
-					double fr = sin(dist(x,y,WIDTH/2,0)/20);
-					double fg = sin(dist(x,y,WIDTH/2,HEIGHT/2)/20);
-					double fb = sin(dist(x,y,0,HEIGHT/2)/20);
+					// Just to make something look ok
+					double v = sin(dist(x,y,WIDTH/2,0)/21) * sin(dist(x,y,WIDTH/2,HEIGHT/2)/20) * sin(dist(x,y,0,HEIGHT/2)/19) * sin(dist(x,y,WIDTH/4,HEIGHT/4)/15);
+					double fr = sin((v * 1) * TAU);
+					double fg = sin((v * 2) * TAU);
+					double fb = sin((v * 3) * TAU);
 
-					byte int(128+128*fb)
-					byte int(128+128*fg)
-					byte int(128+128*fr)
+					byte int(128+128*(fb))
+					byte int(128+128*(fg))
+					byte int(128+128*(fr))
 				}
 
 				// Padding
