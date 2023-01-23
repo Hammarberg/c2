4085d1d3cbb4792b06ebc6df32d2bb047b189c74
John Hammarberg <jh@binarybone.com>
2023-01-22 23:40:28 -0500

    Adding 68k project source for upcoming utilities

---
+++ c2lib/include/c2/motorola/68000.h
@@ -0,0 +1,22 @@
+﻿/*
+	c2 - cross assembler
+	Copyright (C) 2022-2023  John Hammarberg (crt@nospam.binarybone.com)
+
+	This file is part of c2.
+
+	c2 is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+	c2 is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License along with c2. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+#include "c2/h/c2i.h"
+
+class motorola68000 : public c2i
+{
+public:
+	motorola68000(cmdi *pcmd);
+	virtual ~motorola68000();
+};
