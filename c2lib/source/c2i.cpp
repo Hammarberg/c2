fd612780a713620f1a0fb1626ba84ae54c2c054d
Hammarberg <jh@binarybone.com>
2023-06-13 17:59:13 -0400

    Fixed spelling

--- c2lib/source/c2i.cpp
+++ c2lib/source/c2i.cpp
@@ -354,7 +354,7 @@ c2i::c2i(cmdi *pcmd)
 	c2_cmd.declare("--dump-vars", "-V", "[filename]: Output variables. If no filename is given, stdout will be used", 0, 1);
 	c2_cmd.declare("--dump-enum", "-E", "[filename]: Output variables in C-style enum format. If no filename is given, stdout will be used", 0, 1);
 	c2_cmd.declare("--address-range", "-m", "<start> <end>: Set the valid memory address range available for the assembly to target. Addresses must be numerical", 2);
-	c2_cmd.declare("--assembly-hash", "-H", "Verbosly outputs a hash for each assembly step.");
+	c2_cmd.declare("--assembly-hash", "-H", "Verbosely outputs a hash for each assembly step.");
 }
 
 c2i::~c2i()
