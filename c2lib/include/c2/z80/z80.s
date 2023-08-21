4b9fde34d4fe7ead72d248c6b4770323ed5bdc53
jedadams <jed.adams+pub@gmail.com>
2023-08-21 16:39:55 -0400

    more z80 additions

--- c2lib/include/c2/z80/z80.s
+++ c2lib/include/c2/z80/z80.s
@@ -11,9 +11,6 @@
 	You should have received a copy of the GNU General Public License along with c2. If not, see <https://www.gnu.org/licenses/>.
 */
 
-// from here 
-// http://jgmalcolm.com/z80/download/z80.txt 
-
 #pragma once
 
 #define C2_Z80
@@ -36,6 +33,8 @@
 #include "z80_or.s"
 #include "z80_stack.s"
 #include "z80_ret.s"
+#include "z80_sbc.s"
+#include "z80_sub.s"
 
 macro ccf { push8($3f); }
 macro cpd { push16be($eda9,false); }
@@ -58,11 +57,27 @@ macro ldd  { push16be($eda8,false); }
 macro lddr { push16be($edb8,false); }
 macro ldi  { push16be($eda0,false); }
 macro ldir { push16be($edb0,false); }
+macro neg  { push16be($ed44,false); }
+macro nop  { push8($00); }
+macro rla  { push8($17); }
+macro rlca  { push8($07); }
+macro rra  { push8($1f); }
+macro rrca  { push8($0f); }
+macro rrd  { push16be($ed67,false); }
+macro rst 0   { push8($C7); }
+macro rst 8H  { push8($CF); }
+macro rst 10H { push8($D7); }
+macro rst 18H { push8($DF); }
+macro rst 20H { push8($E7); }
+macro rst 28H { push8($EF); }
+macro rst 30H { push8($F7); }
+macro rst 38H { push8($FF); }
+macro rst 38H { push8($FF); }
 
+
+//	commented instructions are TODO 
 
 /*
-NEG           8     2   +1V+++  ED 44
-NOP           4     1   ------  00
 RES b,(HL)    15    2   ------  CB 86+8*b
 RES b,(IX+N)  23    4   ------  DD CB XX 86+8*b
 RES b,(IY+N)  23    4   ------  FD CB XX 86+8*b
@@ -71,45 +86,19 @@ RL (HL)       15    2   +0P0++  CB 16
 RL r          8     2   +0P0++  CB 1r
 RL (IX+N)     23    4   +0P0++  DD CB XX 16
 RL (IY+N)     23    4   +0P0++  FD CB XX 16
-RLA           4     1   +0-0--  17
 RLC (HL)      15    2   +0P0++  CB 06
 RLC (IX+N)    23    4   +0P0++  DD CB XX 06
 RLC (IY+N)    23    4   +0P0++  FD CB XX 06
 RLC r         8     2   +0P0++  CB 0r
-RLCA          4     1   +0-0--  07
 RLD           18    2   -0P0++  ED 6F
 RR (HL)       15    2   +0P0++  CB 1E
 RR r          8     2   +0P0++  CB 18+r
 RR (IX+N)     23    4   +0P0++  DD CB XX 1E
 RR (IY+N)     23    4   +0P0++  FD CB XX 1E
-RRA           4     1   +0-0--  1F
 RRC (HL)      15    2   +0P0++  CB 0E
 RRC (IX+N)    23    4   +0P0++  DD CB XX 0E
 RRC (IY+N)    23    4   +0P0++  FD CB XX 0E
 RRC r         8     2   +0P0++  CB 08+r
-RRCA          4     1   +0-0--  0F
-RRD           18    2   -0P0++  ED 67
-RST 0         11    1   ------  C7
-RST 8H        11    1   ------  CF
-RST 10H       11    1   ------  D7
-RST 18H       11    1   ------  DF
-RST 20H       11    1   ------  E7
-RST 28H       11    1   ------  EF
-RST 30H       11    1   ------  F7
-RST 38H       11    1   ------  FF
-SBC A,(HL)    7     1   +1V+++  9E
-SBC A,(IX+N)  19    3   +1V+++  DD 9E XX
-SBC A,(IY+N)  19    3   +1V+++  FD 9E XX
-SBC A,r       4     1   +1V+++  98+r
-SBC HX              2   +1V+++  DD 9C
-SBC HY              2   +1V+++  FD 9C
-SBC LX              2   +1V+++  DD 9D
-SBC LY              2   +1V+++  FD 9D
-SBC A,N       7     2   +1V+++  DE XX
-SBC HL,BC     15    2   +1V ++  ED 42
-SBC HL,DE     15    2   +1V ++  ED 52
-SBC HL,HL     15    2   +1V ++  ED 62
-SBC HL,SP     15    2   +1V ++  ED 72
 SCF           4     1   10-0--  37
 SET b,(HL)    15    2   ------  CB C6+8*b
 SET b,(IX+N)  23    4   ------  DD CB XX C6+8*b
@@ -131,15 +120,6 @@ SRL (HL)      15    2   +0P0++  CB 3E
 SRL (IX+N)    23    4   +0P0++  DD CB XX 3E
 SRL (IY+N)    23    4   +0P0++  FD CB XX 3E
 SRL r         8     2   +0P0++  CB 38+r
-SUB (HL)      7     1   ++V+++  96
-SUB (IX+N)    19    3   ++V+++  DD 96 XX
-SUB (IY+N)    19    3   ++V+++  FD 96 XX
-SUB r         4     1   ++V+++  9r
-SUB HX              2   ++V+++  DD 94
-SUB HY              2   ++V+++  FD 94
-SUB LX              2   ++V+++  DD 95
-SUB LY              2   ++V+++  FD 95
-SUB N         7     2   ++V+++  D6 XX
 XOR (HL)      7     1   00P0++  AE
 XOR (IX+N)    19    3   00P0++  DD AE XX
 XOR (IY+N)    19    3   00P0++  FD AE XX
