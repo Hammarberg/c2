0f1386391d9fbf1a28152adc51d4aeee49c4d555
John Hammarberg <jh@binarybone.com>
2023-10-17 12:42:45 -0400

    removing parantesis

--- c2lib/include/c2/c64/c64_sid.s
+++ c2lib/include/c2/c64/c64_sid.s
@@ -15,10 +15,10 @@
 
 // Base address
 #define SID_BASE 	$d400
-#define SID_V1		(SID_BASE+$0)
-#define SID_V2		(SID_BASE+$7)
-#define SID_V3		(SID_BASE+$e)
-#define SID_X		(SID_BASE+$15)
+#define SID_V1		SID_BASE+$0
+#define SID_V2		SID_BASE+$7
+#define SID_V3		SID_BASE+$e
+#define SID_X		SID_BASE+$15
 
 // Voice 1-3
 #define SID_FREQ	0
@@ -43,46 +43,46 @@
 #define SID_OADSR   7
 
 // Absolute addresses
-#define SID_V1_FREQ	    (SID_V1+SID_FREQ)
-#define SID_V1_FREQL    (SID_V1+SID_FREQL)
-#define SID_V1_FREQH	(SID_V1+SID_FREQH)
-#define SID_V1_PW       (SID_V1+SID_PW)
-#define SID_V1_PWL      (SID_V1+SID_PWL)
-#define SID_V1_PWH      (SID_V1+SID_PWH)
-#define SID_V1_CTRL     (SID_V1+SID_CTRL)
-#define SID_V1_AD       (SID_V1+SID_AD)
-#define SID_V1_SR       (SID_V1+SID_SR)
+#define SID_V1_FREQ	    SID_V1+SID_FREQ
+#define SID_V1_FREQL    SID_V1+SID_FREQL
+#define SID_V1_FREQH	SID_V1+SID_FREQH
+#define SID_V1_PW       SID_V1+SID_PW
+#define SID_V1_PWL      SID_V1+SID_PWL
+#define SID_V1_PWH      SID_V1+SID_PWH
+#define SID_V1_CTRL     SID_V1+SID_CTRL
+#define SID_V1_AD       SID_V1+SID_AD
+#define SID_V1_SR       SID_V1+SID_SR
 
-#define SID_V2_FREQ     (SID_V2+SID_FREQ)
-#define SID_V2_FREQL	(SID_V2+SID_FREQL)
-#define SID_V2_FREQH	(SID_V2+SID_FREQH)
-#define SID_V2_PW       (SID_V2+SID_PW)
-#define SID_V2_PWL      (SID_V2+SID_PWL)
-#define SID_V2_PWH      (SID_V2+SID_PWH)
-#define SID_V2_CTRL     (SID_V2+SID_CTRL)
-#define SID_V2_AD       (SID_V2+SID_AD)
-#define SID_V2_SR       (SID_V2+SID_SR)
+#define SID_V2_FREQ     SID_V2+SID_FREQ
+#define SID_V2_FREQL	SID_V2+SID_FREQL
+#define SID_V2_FREQH	SID_V2+SID_FREQH
+#define SID_V2_PW       SID_V2+SID_PW
+#define SID_V2_PWL      SID_V2+SID_PWL
+#define SID_V2_PWH      SID_V2+SID_PWH
+#define SID_V2_CTRL     SID_V2+SID_CTRL
+#define SID_V2_AD       SID_V2+SID_AD
+#define SID_V2_SR       SID_V2+SID_SR
 
-#define SID_V3_FREQ     (SID_V3+SID_FREQ)
-#define SID_V3_FREQL	(SID_V3+SID_FREQL)
-#define SID_V3_FREQH	(SID_V3+SID_FREQH)
-#define SID_V3_PW       (SID_V3+SID_PW)
-#define SID_V3_PWL      (SID_V3+SID_PWL)
-#define SID_V3_PWH      (SID_V3+SID_PWH)
-#define SID_V3_CTRL     (SID_V3+SID_CTRL)
-#define SID_V3_AD       (SID_V3+SID_AD)
-#define SID_V3_SR       (SID_V3+SID_SR)
+#define SID_V3_FREQ     SID_V3+SID_FREQ
+#define SID_V3_FREQL	SID_V3+SID_FREQL
+#define SID_V3_FREQH	SID_V3+SID_FREQH
+#define SID_V3_PW       SID_V3+SID_PW
+#define SID_V3_PWL      SID_V3+SID_PWL
+#define SID_V3_PWH      SID_V3+SID_PWH
+#define SID_V3_CTRL     SID_V3+SID_CTRL
+#define SID_V3_AD       SID_V3+SID_AD
+#define SID_V3_SR       SID_V3+SID_SR
 
 // Extra registers
-#define SID_X_FCOFF     (SID_X+SID_FCOFF)
-#define SID_X_FCOFFL    (SID_X+SID_FCOFFL)
-#define SID_X_FCOFFH    (SID_X+SID_FCOFFH)
-#define SID_X_FCTRL     (SID_X+SID_FCTRL)
-#define SID_X_VOL       (SID_X+SID_VOL)
-#define SID_X_PADX      (SID_X+SID_PADX)
-#define SID_X_PADY      (SID_X+SID_PADY)
-#define SID_X_OWAV      (SID_X+SID_OWAV)
-#define SID_X_OADSR     (SID_X+SID_OADSR)
+#define SID_X_FCOFF     SID_X+SID_FCOFF
+#define SID_X_FCOFFL    SID_X+SID_FCOFFL
+#define SID_X_FCOFFH    SID_X+SID_FCOFFH
+#define SID_X_FCTRL     SID_X+SID_FCTRL
+#define SID_X_VOL       SID_X+SID_VOL
+#define SID_X_PADX      SID_X+SID_PADX
+#define SID_X_PADY      SID_X+SID_PADY
+#define SID_X_OWAV      SID_X+SID_OWAV
+#define SID_X_OADSR     SID_X+SID_OADSR
 
 // Voice control register flags
 #define SID_ON			1
