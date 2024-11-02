eed9bc5df2197cff3c0d1394bd31903cd984f891
John Hammarberg <jh@binarybone.com>
2024-11-02 00:30:08 -0400

    Adding subasm example
    Demonstrates a hires image on C64, import and assemble features

---
+++ examples/c64/subasm/showimg.cpp
@@ -0,0 +1,36 @@
+#include "c2/c64/c64.h"
+
+class c2_project_showimg : public c64
+{
+public:
+	c2_project_showimg(cmdi *pcmd):c64(pcmd){}
+
+C2_SECTION_TOP
+
+	void c2_pass() override
+	{
+		C2_SECTION_ASM
+		{
+			#include "showimg.s"
+		}
+	}
+
+	void c2_pre() override
+	{
+		c64::c2_pre();
+		// Insert custom pre-pass code here
+	}
+
+	void c2_post() override
+	{
+		c64::c2_post();
+		// Insert custom post-pass code here
+	}
+
+	const char *c2_get_template() override
+	{
+		return "c64vice";
+	}
+};
+
+c2i *c2_create_object_instance(cmdi *pcmd) {return new c2_project_showimg(pcmd);}
---
+++ examples/c64/subasm/showimg.s
@@ -0,0 +1,43 @@
+#include "c2/c64/c64.s"
+#include "c2/mos/6502.s"
+
+			// showimg
+
+			macro set_d011 @vscroll, @rows25, @screen_on, @bitmap, @extended ,@raster_hi
+			{
+				lda #(vscroll&7) | ((rows25&1) << 3) | ((screen_on&1) << 4) | ((bitmap&1) << 5) | ((extended&1) << 6) | ((raster_hi&1) << 7)
+				sta $d011
+			}
+
+			macro set_d018 @font, @screen
+			{
+				lda # (((screen&$3fff)/$0400) << 4) | (((font&$3fff)/$0800) << 1)
+				sta $d018
+			}
+
+			@ = $0900
+			basic_startup
+			
+			sei
+
+			lda #$03
+			sta $d020
+
+			set_d011 3, 1, 1, 1, 0, 0
+			/* expands to
+			lda #$3b
+			sta $d011
+			*/
+
+			set_d018 bitmap, screen
+			/* expands to
+			lda #$38
+			sta $d018
+			*/
+
+:			jmp -
+
+			//import labels from bitmapsdata.s
+			import bitmap
+			import screen
+			assemble "bitmapdata.s"
